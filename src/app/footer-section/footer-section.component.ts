f0473012a84bca17d0da055fef7d69fdf7c6573c
chaknogf <chaknogf@gmail.com>
2025-07-07 06:44:22 -0600

    style: diseño de elementos

--- src/app/footer-section/footer-section.component.ts
+++ src/app/footer-section/footer-section.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { logoSVG } from '../shared/logo';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer-section',
@@ -8,4 +10,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './footer-section.component.html',
   styleUrls: ['./footer-section.component.css']
 })
-export class FooterSectionComponent { }
+export class FooterSectionComponent {
+    logoicon: SafeHtml = logoSVG
+  link: string = 'contacto@clinicalossables.com';
+  isMenuOpen = false;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.logoicon = this.sanitizer.bypassSecurityTrustHtml(logoSVG);
+  }
+
+
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
